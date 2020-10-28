c6f216adc3d0a1ce23cb1c54a304ba41fb70395d
NRK <49558826+notRoyKim@users.noreply.github.com>
2020-10-28 21:00:09 +0900

    Initial commit

---
+++ src/main/java/proj/advweb/tabletopInfo/TabletopInfoApplication.java
@@ -0,0 +1,13 @@
+package proj.advweb.tabletopInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletopInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proj/advweb/tabletopInfo/TabletopInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.advweb.tabletopInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabletopInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
